2d29b23318ebdcaeda34d34079195a099693d413
dlepke <deannalepke@gmail.com>
2022-05-05 15:34:32 -0700

    got login working, set up a server

---
+++ public/scripts/login.js
@@ -0,0 +1,4 @@
+function login(username, password) {
+    console.log("username is " + username);
+    console.log("password is " + password);
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require("cookie-parser");
+var session = require('express-session');
+const bodyParser = require('body-parser');
+
+var publicPath = path.join(__dirname, 'public');
+var htmlPath = path.join(__dirname, 'public/HTML');
+
+app.use(express.static('public'));
+app.use(cookieParser());
+
+const correctUsername = "hi";
+const correctPassword = "hello";
+
+const oneDay = 1000 * 60 * 60 * 24;
+
+app.use(session({
+    secret: 'shush',
+    saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: oneDay }
+}));
+
+app.listen(5050, function(err) {
+    if (err) {
+        console.log(err);
+    }
+})
+
+app.get('/', function (req, res, next) {
+    if (req.session.username) {
+        res.send("logged in :)");
+    } else {
+        res.sendFile(path.join(htmlPath + '/sign_in.html'));
+    }
+})
+
+app.get("/profile", function (req, res, next) {
+    console.log("getting profile");
+    if (req.session.username) {
+        res.sendFile(path.join(htmlPath + '/user_profile.html'));
+    } else {
+        res.redirect('/');
+    }
+})
+
+app.post('/authenticate',
+    bodyParser.urlencoded(),
+    (req, res, next) => {
+        console.log("req: ", req.body)
+        if (req.body.username == correctUsername && req.body.password == correctPassword) {
+            res.locals.username = req.body.username;
+            console.log("correct username and password");
+            next();
+        }
+    },
+    (req, res) => {
+        req.session.loggedIn = true;
+        req.session.username = res.locals.username;
+        console.log("session: " + req.session);
+        res.redirect('/profile');
+        res.send();
+    }
+)
