173b2d1baf4859fef52edc391995dbdecd4071d9
Matthew <Matirix@live.com>
2022-05-26 11:15:03 -0700

    modifided to input email, confirmation box for update item, as well as informative feedback for WL

--- public/scripts/update_item.js
+++ public/scripts/update_item.js
@@ -10,7 +10,9 @@ function updateItem() {
     price_required = document.getElementById("new_price")
     store_required = document.getElementById("store_required")
     number_required = document.getElementById("number_required")
+    confirmation_required = document.getElementById("confirmation_unsucessful")
     item_updated_successfully = document.getElementById("item_updated_successfully")
+
 
     if (itemName.trim().length === 0) {
         price_required.style.visibility = "hidden";
@@ -41,6 +43,14 @@ function updateItem() {
         number_required.style.visibility = "visible";
         $("#price").css({ "border": '#FF0000 1px solid'});
 
+    } else if (!document.getElementById('confirm_yes').checked) {
+        name_required.style.visibility = "hidden";
+        store_required.style.visibility = "hidden";
+        price_required.style.visibility = "hidden";
+        item_updated_successfully.style.visibility = "hidden";
+        number_required.style.visibility = "hidden";
+        $("#confirm_box").css({ "border": '#FF0000 1px solid'});
+        confirmation_required.style.visibility = "visible";
     } else {
         $.post('/update_item', {
             itemName: itemName,
@@ -51,6 +61,9 @@ function updateItem() {
         store_required.style.visibility = "hidden";
         price_required.style.visibility = "hidden";
         number_required.style.visibility = "hidden";
+        $("#confirmation_unsucessful").hide();
         item_updated_successfully.style.visibility = "visible";
+
+
     }
 }
